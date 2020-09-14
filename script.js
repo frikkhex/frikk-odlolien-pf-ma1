e5f9c81df2ec2a6153e436b1689e7fd10ec44002
frikkhex <67097261+frikkhex@users.noreply.github.com>
2020-09-14 15:14:40 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// 1. Declare a variable called firstVar but don't initialise it with a value.
+// var firstVar;
+
+// 2. Declare a variable called name and assign it your first name.
+// var name = "Frikk";
+
+// 3. Declare and initialise a variable with a number value.
+// var age = 28;
+
+// 4. Create a variable called division and initialise it with a value of 20 divided by 5.
+// var division = 20 / 5;
+
+// 5. Write code that checks the type of the value "frog".
+// typeof "frog";
+
+// 6. Declare and initialise a variable called orderHasShipped with a boolean value.
+// var orderHasShipped = true;
+
+// 7. Create an if statement that checks if orderHasShipped is true. If it is true, console log the string value "true". If not, console log the string value "false".
+// if (orderHasShipped === true) {
+//  console.log("true");
+// } else {
+//  console.log("false");
+// }
+
+// 8. Create a for loop that counts from 0 to 9. Console log the value of the counter variable inside the loop.
+// for (var i = 0; i < 10; i++) {
+//  console.log(i);
+// }
